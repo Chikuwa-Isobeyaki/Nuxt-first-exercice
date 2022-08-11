ed2b503597fbd2a473e2a84e88dbc25cc276ba1e
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-11 23:12:58 +0900

    [Finish]画面遷移演習

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+      <p>Top Page</p>
+      <hr>
+      <router-link to="/mypage">My Page</router-link>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+    }
+  }
+}
+</script>
---
+++ pages/mypage.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+      <p>My Page</p>
+      <hr>
+      <router-link to="/">Top Page</router-link>
+  </section>
+</template>
+
+
+<script>
+export default {}
+</script>
